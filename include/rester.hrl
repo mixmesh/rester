ce9ef254b9d4f32f5cf122142c0e2aae08065888
Joakim G <joagre@gmail.com>
2020-10-20 21:25:09 +0200

    TODO item fixed: "Log debug format. Conditional call."

--- include/rester.hrl
+++ include/rester.hrl
@@ -1,7 +1,7 @@
 -ifndef(_RESTER_HRL_).
 -define(_RESTER_HRL_, true).
 
--include_lib("apptools/include/log.hrl").
+-include_lib("obscrete/include/log.hrl").
 
 -type path() :: string().
 -type user() :: binary().
@@ -11,7 +11,7 @@
 		      {integer(),integer(),integer(),integer(),integer(),integer()}.
 -type cred() :: {basic,path(),user(),password(),realm()} |
 		{digest,path(),user(),password(),realm()}. %% Old type
--type guard() :: ip_address() | 
+-type guard() :: ip_address() |
 		 {ip_address(), integer()} |
 		 afunix |
 		 http |
@@ -21,36 +21,34 @@
 
 -define(RESTER_DEBUG, true).
 
-%% -ifdef(RESTER_DEBUG).
-%% -define(log_debug(F,A), ?dbg_log(io_lib:format((F),(A)))).
-%% -define(log_debug(F), ?dbg_log(io_lib:format((F),[]))).
-%% -else.
-%% -define(log_debug(F,A), ok).
-%% -define(log_debug(F), ok).
-%% -endif.
-
-%% -define(log_warning(F,A), ?daemon_tag_log(warn,(F),(A))).
-%% -define(log_warning(F), ?daemon_tag_log(warn,(F),[])).
-%% -define(log_error(F,A), ?daemon_tag_log(error,(F),(A))).
-%% -define(log_error(F), ?daemon_tag_log(error,(F),[])).
-%% -define(log_info(F,A), ?daemon_tag_log(info, (F),(A))).
-%% -define(log_info(F), ?daemon_tag_log(info, (F),[])).
-
-
 -ifdef(RESTER_DEBUG).
--define(log_debug(F,A), io:format((F)++"\n",(A))).
--define(log_debug(F),  io:format((F)++"\n",[])).
+-define(log_debug(F,A), ?dbg_log_fmt((F), (A))).
+-define(log_debug(F),  ?dbg_log_fmt((F), [])).
 -else.
 -define(log_debug(F,A), ok).
 -define(log_debug(F), ok).
 -endif.
 
--define(log_warning(F,A), io:format("warning: "++(F)++"\n",(A))).
--define(log_warning(F), io:format("warning: "++(F)++"\n",[])).
--define(log_error(F,A), io:format("error: "++(F)++"\n",(A))).
--define(log_error(F), io:format("error: "++(F)++"\n",[])).
--define(log_info(F,A), io:format("info: "++(F)++"\n",(A))).
--define(log_info(F), io:format("info: "++(F)++"\n",[])).
+-define(log_warning(F,A), ?daemon_log_tag_fmt(warning, (F), (A))).
+-define(log_warning(F), ?daemon_log_tag_fmt(warning, (F), [])).
+-define(log_error(F,A), ?dbg_log_tag_fmt(error, (F), (A))).
+-define(log_error(F), ?daemon_log_tag_fmt(error, (F), [])).
+-define(log_info(F,A), ?daemon_log_tag_fmt(info, (F), (A))).
+-define(log_info(F), ?daemon_log_tag_fmt(info, (F), [])).
+
+%% -ifdef(RESTER_DEBUG).
+%% -define(log_debug(F,A), io:format((F)++"\n",(A))).
+%% -define(log_debug(F),  io:format((F)++"\n",[])).
+%% -else.
+%% -define(log_debug(F,A), ok).
+%% -define(log_debug(F), ok).
+%% -endif.
+
+%% -define(log_warning(F,A), io:format("warning: "++(F)++"\n",(A))).
+%% -define(log_warning(F), io:format("warning: "++(F)++"\n",[])).
+%% -define(log_error(F,A), io:format("error: "++(F)++"\n",(A))).
+%% -define(log_error(F), io:format("error: "++(F)++"\n",[])).
+%% -define(log_info(F,A), io:format("info: "++(F)++"\n",(A))).
+%% -define(log_info(F), io:format("info: "++(F)++"\n",[])).
 
 -endif.
-
